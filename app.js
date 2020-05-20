80c39a1d871787c73175dbd16e4276a2f077a877
leo <xiaoxinxin101@outlook.com>
2020-05-21 05:03:34 +0800

    初始化路由 public文件用域存放图片样式等 views则是视图模板 将路由中的方法抽离出来放在控制器

---
+++ app.js
@@ -0,0 +1,28 @@
+const Koa = require('koa');
+const static = require('koa-static');
+const views = require('koa-views');
+const router = require('./routers/index');
+const logger = require('koa-logger');
+const body = require('koa-body');
+const { join } = require('path');
+
+//生成koa实例
+const app = new Koa;
+
+
+//挂载
+app
+    .use(logger()) //注册日志模块 打印日志 主要是服务器请求与响应所耗费的时间和内存
+    .use(static(join(__dirname, "public"))) //配置静态资源目录
+    .use(views(join(__dirname, "views"), { //配置视图模板
+        extension: "pug"
+    }))
+    .use(body()) // 配置koa-body 处理post请求数据
+
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods())
+    .listen(3000, () => {
+        console.log("服务器已启动");
+    })
---
+++ controller/user.js
@@ -0,0 +1,3 @@
+exports.fn = async ctx => {
+    ctx.body = "啊哈哈"
+}
